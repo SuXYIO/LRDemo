96fec6e978ff990de0f1b6ca595b00098e4621f0
SuXYIO <SuXYMC@163.com>
2024-06-07 18:12:44 +0800

    Add new feature to plot.py. Update README.md.

--- plot.py
+++ plot.py
@@ -1,24 +1,36 @@
 '''
 LinearRegressionPlot
 Simple example of using the output CSV file of LR to plot.
+
+Usage
+    python3 plot.py [filepath]
+        "filepath" is the path to the CSV formatted file LR generated. 
+        If "filepath" empty, the program will search in the current dir for "*.csv". 
+        It uses the only one if there is only one "*.csv" file. 
+        It prompts the user which one to use if there is more than one or no "*.csv" file. 
 '''
 import os
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 
-PATTERN = ".csv"
-csvfiles = []
-for f in os.listdir("."):
-    if f.endswith(PATTERN):
-        csvfiles.append(f)
-if len(csvfiles) > 1:
-    # muti csv, ask for one
-    csvfilename = input(f'Found mutiple CSV files: \n{print(csvfiles)}\nChoose one CSV file\n> ')
-elif len(csvfiles) == 1:
-    csvfilename = csvfiles[0]
+if sys.argv[1] != '':
+    csvfilename = sys.argv[1]
 else:
-    # no csv
-    csvfilename = input('Enter filepath\n> ')
+    PATTERN = ".csv"
+    csvfiles = []
+    for f in os.listdir("."):
+        if f.endswith(PATTERN):
+            csvfiles.append(f)
+    if len(csvfiles) > 1:
+        # muti csv, ask for one
+        print(f'Found mutiple CSV files: \n{print(csvfiles)}\n')
+        csvfilename = input('Choose one CSV file\n> ')
+    elif len(csvfiles) == 1:
+        csvfilename = csvfiles[0]
+    else:
+        # no csv
+        csvfilename = input('Enter filepath\n> ')
 
 df = pd.read_csv(csvfilename)
 ln = df.index
