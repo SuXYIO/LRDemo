1ad00832178c3daee90e9da7146c14fd2f43abe0
SuXY <SuXYMC@163.com>
2024-07-31 18:23:04 +0800

    Update signal handler.

--- signal.c
+++ signal.c
@@ -1,9 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <unistd.h>
+#include <termios.h>
 
 void sighandler(int sig) {
-	printf("%s\nSignal %d received, terminating...\n%s", COLOR_WARN, sig, COLOR_END);
-	exit(sig);
+	printf("%s\nSignal %d received\n%s", COLOR_WARN, sig, COLOR_END);
+	printf("%sQuit[y/n]> %s", COLOR_WARN, COLOR_END);
+	fflush(stdout);
+	char c = '\0';
+	read(STDIN_FILENO, &c, 1);
+	printf("%c\n", c);
+	fflush(stdout);
+	if (c == 'y')
+		exit(sig);
+	else
+		return;
 }
